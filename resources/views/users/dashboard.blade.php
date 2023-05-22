2135661b1f197b3d1a6ab72f4ca6727d233ea1f2
ikursus.com <ikursus.com@gmail.com>
2023-05-22 14:59:57 +0800

    Update template

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -5,9 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
   </head>
   <body>
-    <h1>Dashboard</h1>
+    <div class="container">
+        <h1>Senarai Users</h1>
+
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>NAMA</th>
+                    <th>EMAIL</th>
+                    <th>ROLE</th>
+                    <th>NO PEKERJA</th>
+                    <th>STATUS</th>
+                    <th>TINDAKAN</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 // Route pelawat
@@ -16,3 +17,15 @@ Route::post('login', [LoginController::class, 'authenticate'])->name('login.auth
 Route::middleware('auth')->group(function() {
     Route::get('dashboard', fn() => view('user.dashboard'));
 });
+
+// CRUD
+// Route::get('/users', [UserController::class, 'index']);
+// Route::get('/users/add', [UserController::class, 'create']);
+// Route::post('/users/add', [UserController::class, 'store']);
+// Route::post('/users/{id}', [UserController::class, 'show']);
+// Route::get('/users/{id}/edit', [UserController::class, 'edit']);
+// Route::patch('/users/{id}/edit', [UserController::class, 'update']);
+// Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+// Route::resource('users', UserController::class)->only(['create', 'store', 'show']);
+Route::resource('users', UserController::class);
