2664fd01799b75e884f49c11368136e679c4d3da
ikursus.com <ikursus.com@gmail.com>
2023-05-26 12:02:42 +0800

    Update broadcasting

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -43,5 +43,62 @@
 @endsection
 
 @push('scripting')
+<script src="https://js.pusher.com/7.2/pusher.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
 
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher('{{ config('broadcasting.connections.pusher.key') }}', {
+      cluster: '{{ config('broadcasting.connections.pusher.options.cluster') }}'
+    });
+
+    var channel = pusher.subscribe('public');
+
+    channel.bind('chat', function(data) {
+        $.post("{{ route('chat.receive') }}", {
+            _token: "{{ csrf_token() }}",
+            message: data.message,
+        })
+        .done(function(respon) {
+            // Append mesej yang diterima dari mesej sebelumnya
+            $(".message > .message").last().after(respon);
+            // Scroll content selepas send supaya mesej yang terkumpul boleh dibaca
+            $(document).scrollTop((document).height());
+        });
+
+        alert(JSON.stringify(data));
+    });
+
+    // Hantar mesej kepada route send supaya dibroadcast ke pusher
+    $("form").submit(function (event) {
+
+        // Elakkan form page reload submit
+        event.preventDefault();
+
+        $.ajax({
+            url: "{{ route('chat.send') }}",
+            method: "POST",
+            headers: {
+                'X-Socket-Id': pusher.connection.socket_id
+            },
+            data: {
+                _token: "{{ csrf_token() }}",
+                message: $("form #message").val(),
+            }
+        }).done(function( respon ) {
+            // Append mesej yang diterima dari mesej sebelumnya
+            $(".message > .message").last().after(respon);
+            // Clearkan input message selepas send
+            $("form #message").val();
+            // Scroll content selepas send supaya mesej yang terkumpul boleh dibaca
+            $(document).scrollTop((document).height());
+        });
+
+    });
+
+
+
+  </script>
 @endpush
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -18,6 +18,9 @@
 <li class="nav-item">
     <a class="nav-link" href="{{ route('tokens.index') }}">Access Token</a>
 </li>
+<li class="nav-item">
+    <a class="nav-link" href="{{ route('chat.index') }}">Chat</a>
+</li>
 </ul>
 {{-- <form class="d-flex" role="search">
 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
